8187d91fa134742c640415f6ce5e4a8295eef65a
Florian Schulze <florian.schulze@gmx.net>
2015-11-19 13:05:20 +0100

    Add minimum version which introduced the fix for password logging.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     install_requires=[
         'PyYAML',
         'devpi-server>=2.0.0',
-        'ldap3'],
+        'ldap3>=0.9.8.6'],
     include_package_data=True,
     zip_safe=False,
     packages=['devpi_ldap'])
