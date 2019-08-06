52796ad0473d9e421838b907df7cea9e2c7e2b75
Graham Williamson <graham@williamsonsinc.id.au>
2019-08-06 19:48:50 +1000

    Fixes permission issue writing file for root user

--- scripts/setup-389-ds.sh
+++ scripts/setup-389-ds.sh
@@ -5,7 +5,8 @@ whoami
 
 sudo apt-get install 389-ds-base
 
-cat <<EOF > /root/instance.inf
+
+cat << EOF | sudo tee /root/instance.inf
 # /root/instance.inf
 [general]
 config_version = 2
@@ -18,11 +19,11 @@ sample_entries = yes
 suffix = dc=example,dc=com
 EOF
 
-dscreate from-file /root/instance.inf
+sudo dscreate from-file /root/instance.inf
 
 dsctl localhost status
 
-cat << EOF > /root/.dsrc
+cat << EOF | sudo tee /root/.dsrc
 [localhost]
 # Note that '/' is replaced to '%%2f'.
 uri = ldapi://%%2fvar%%2frun%%2fslapd-localhost.socket
@@ -30,8 +31,8 @@ basedn = dc=example,dc=com
 binddn = cn=Directory Manager
 EOF
 
-dsidm localhost user create --uid eve --cn Eve --displayName 'Eve User - Devpi test' --uidNumber 1001 --gidNumber 1001 --homeDirectory /home/eve
-dsidm localhost user create --uid alice --cn Alice --displayName 'Alice User - Devpi test' --uidNumber 1002 --gidNumber 1002 --homeDirectory /home/alice
+sudo dsidm localhost user create --uid eve --cn Eve --displayName 'Eve User - Devpi test' --uidNumber 1001 --gidNumber 1001 --homeDirectory /home/eve
+sudo dsidm localhost user create --uid alice --cn Alice --displayName 'Alice User - Devpi test' --uidNumber 1002 --gidNumber 1002 --homeDirectory /home/alice
 
-dsidm localhost group create --cn devpi_admins
-dsidm localhost group add_member devpi_admins uid=alice,ou=people,dc=example,dc=com
+sudo dsidm localhost group create --cn devpi_admins
+sudo dsidm localhost group add_member devpi_admins uid=alice,ou=people,dc=example,dc=com
