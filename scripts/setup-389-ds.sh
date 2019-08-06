35b329a1d2ebf8fff09830603c2ec9b62aa0132e
Graham Williamson <graham@williamsonsinc.id.au>
2019-08-06 17:31:12 +1000

    Adds 389-ds setup to travis

---
+++ scripts/setup-389-ds.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -eo
+
+apt-get install 389-ds-base
+
+cat <<EOF > /root/instance.inf
+# /root/instance.inf
+[general]
+config_version = 2
+
+[slapd]
+root_password = YOUR_ADMIN_PASSWORD_HERE
+
+[backend-userroot]
+sample_entries = yes
+suffix = dc=example,dc=com
+EOF
+
+dscreate from-file /root/instance.inf
+
+dsctl localhost status
+
+cat << EOF > /root/.dsrc
+[localhost]
+# Note that '/' is replaced to '%%2f'.
+uri = ldapi://%%2fvar%%2frun%%2fslapd-localhost.socket
+basedn = dc=example,dc=com
+binddn = cn=Directory Manager
+EOF
+
+whoami
+
+dsidm localhost user create --uid eve --cn Eve --displayName 'Eve User - Devpi test' --uidNumber 1001 --gidNumber 1001 --homeDirectory /home/eve
+dsidm localhost user create --uid alice --cn Alice --displayName 'Alice User - Devpi test' --uidNumber 1002 --gidNumber 1002 --homeDirectory /home/alice
+
+dsidm localhost group create --cn devpi_admins
+dsidm localhost group add_member devpi_admins uid=alice,ou=people,dc=example,dc=com
